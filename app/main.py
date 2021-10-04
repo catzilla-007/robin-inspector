4ef2e544d7924751d8c2e7663d2fd6d94ba7a4e6
catzilla-007 <1916652+catzilla-007@users.noreply.github.com>
2021-10-04 11:44:43 +0800

    Dev (#7)
    * Endpoint for water-level (#2)
    
    * ROBIN-1: Add environment variables
    
    * ROBIN-1: Add working water-level endpoint
    
    * General architecture (#3)
    
    * Change web framework from Flask to FastAPI
    
    * Add water level GET request
    
    * General architecture (#4)
    
    * Change web framework from Flask to FastAPI
    
    * Add water level GET request
    
    * Add initial routing architecture
    
    * Test (#6)
    
    * Change web framework from Flask to FastAPI
    
    * Add water level GET request
    
    * Add initial routing architecture
    
    * Add sensors package and improve v1 routing

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
 from fastapi import FastAPI
+from app import v1
 
 api = FastAPI()
 
-
-@api.get('/api/v1/nft/{nft_id}/water-level')
-def get_water_level(nft_id: int):
-    return {
-        'timestamp': '2021-03-22 10:22:11',
-        'type': 'water-level',
-        'nft': nft_id,
-        'value': 'normal',
-    }
+api.include_router(v1.router)
